259858938f219c0cf6edb014312aa2ab3ed72113
kbefff <kbeffel10@me.com>
2019-04-30 07:27:34 -0700

    update profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -69,18 +69,58 @@ router.post('/', [
     const profileFields = {};
 
     profileFields.user = req.user.id;
-    if(company) profileFields.company = company;
-    if(website) profileFields.website = website;
-    if(location) profileFields.location = location;
-    if(bio) profileFields.bio = bio;
-    if(status) profileFields.status = status;
-    if(githubusername) profileFields.githubusername = githubusername;
-    if(skills) {
-        profileFields.skills = skills.split(',').map(skill => skill.trim());
+    if (company) 
+        profileFields.company = company;
+    if (website) 
+        profileFields.website = website;
+    if (location) 
+        profileFields.location = location;
+    if (bio) 
+        profileFields.bio = bio;
+    if (status) 
+        profileFields.status = status;
+    if (githubusername) 
+        profileFields.githubusername = githubusername;
+    if (skills) {
+        profileFields.skills = skills
+            .split(',')
+            .map(skill => skill.trim());
     }
-    console.log(profileFields.skills);
 
-    res.send('Hello');
+    // Build Social Object
+    profileFields.social = {};
+    if (youtube) 
+        profileFields.social.youtube = youtube;
+    if (facebook) 
+        profileFields.social.facebook = facebook;
+    if (twitter) 
+        profileFields.social.twitter = twitter;
+    if (instagram) 
+        profileFields.social.instagram = instagram;
+    if (linkedin) 
+        profileFields.social.linkedin = linkedin;
+    
+    try {
+        let profile = await Profile.findOne({user: req.user.id});
+
+        if (profile) {
+            // Update
+            profile = await Profile.findOneAndUpdate({
+                user: req.user.id
+            }, {
+                $set: profileFields
+            }, {new: true});
+            return res.json(profile);
+        }
+
+        // Create
+
+    } catch (err) {
+        console.error(err.message);
+        res
+            .status(500)
+            .send('Server Error');
+    }
 })
 
 module.exports = router;
