f7e7d6c4b59bc82875f0edff6090e54490f7c640
kbefff <kbeffel10@me.com>
2019-04-29 19:31:57 -0700

    mongo connection

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+// import {connectDB} from ('./config/db')
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.get('/', (req,res) => res.send('API Running'))
+// Connect database
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+// Connect to the Mongo DB
+mongoose.connect(db, {useNewUrlParser: true});
+
+app.get('/', (req, res) => res.send('API Running'));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
