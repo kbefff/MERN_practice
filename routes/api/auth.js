b1c4f46d6b3c0d722e482275f258a94adb79db4a
kbefff <kbeffel10@me.com>
2019-04-29 21:09:09 -0700

    send bck user data

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,9 +2,21 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 
+const User = require('../../models/user')
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
-router.get('/', auth, (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+    try{
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user)
+
+    } catch(err){
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+
+
+});
 
 module.exports = router;
