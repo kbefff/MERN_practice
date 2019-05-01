f7cbd8b5fc1b27a4c5e437b553f231fcb3dddd60
kbefff <kbeffel10@me.com>
2019-05-01 13:40:07 -0700

    get profile by user ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -127,4 +127,46 @@ router.post('/', [
     }
 })
 
+// @route   Get api/profile 
+// @desc    Get all profiles
+// @access  Public
+
+router.get('/', async (req, res) =>{
+    try {
+        // find all profiles. Add names and Avatar which are part of the 'user' collection (defined in the model)
+        const profiles = await Profile.find().populate('user', ['name', 'avatar'])
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res
+            .status(500)
+            .send('Server Error');
+    }
+});
+
+// @route   Get api/profile/user/:user_id 
+// @desc    Get profile by user ID
+// @access  Public
+
+router.get('/user/:user_id', async (req, res) =>{
+    try {
+        // find all profiles. Add names and Avatar which are part of the 'user' collection (defined in the model)
+        const profile = await Profile.findOne({user : req.params.user_id}).populate('user', ['name', 'avatar']);
+        // if no profile is created send error
+        if (!profile) return res.status(400).json({msg: "Profile not found"});
+
+        res.json(profile);
+
+        // catch will run for any invalid object IDs
+    } catch (err) {
+        console.error(err.message);
+
+        if(err.kind == 'ObjectId'){
+            return res.status(400).json({msg: "Profile not found"})
+        }
+        res
+            .status(500)
+            .send('Server Error');
+    }
+});
 module.exports = router;
