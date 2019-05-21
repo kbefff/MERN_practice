11a26d37252f3ddeba48ff37a2507d7b61d3f8cb
kbefff <kbeffel10@me.com>
2019-05-21 15:58:30 -0700

    auth

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,11 @@
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL
+} from '../actions/types';
+
+
+const initialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    loading: true
+}
