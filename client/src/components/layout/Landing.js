68ac402e200daf56584390728f92f1445083897f
kbefff <kbeffel10@me.com>
2019-05-20 19:44:00 -0700

    login and register front end

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Landing = () => {
+    return (
+        <section className="landing">
+            <div className="dark-overlay">
+                <div class="landing-inner">
+                    <h1 class="x-large">Developer Connector</h1>
+                    <p class="lead">
+                        Create a developer profile/portfolio, share posts and get help from other
+                        developers
+                    </p>
+                    <div class="buttons">
+                        <a href="register.html" className="btn btn-primary">Sign Up</a>
+                        <a href="login.html" className="btn btn-light">Login</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Landing;
