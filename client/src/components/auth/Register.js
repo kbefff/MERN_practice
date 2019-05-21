9a33f326dd7ebf94126cba850435b82ad82d8ced
kbefff <kbeffel10@me.com>
2019-05-21 11:49:29 -0700

    register page

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, {Fragment, useState} from "react";
 
 const Register = () => {
-    return (
-       <div>Register</div>
-    )
-}
+    const [formData,
+        setFormData] = useState({name: '', email: '', password: '', password2: ''});
+
+    const {name, email, password, password2} = formData;
+
+    return <Fragment>
+        <h1 class="large text-primary">Sign Up</h1>
+        <p class="lead">
+            <i class="fas fa-user"></i>
+            Create Your Account</p>
+        <form class="form" action="create-profile.html">
+            <div class="form-group">
+                <input type="text" placeholder="Name" name="name" required/>
+            </div>
+            <div class="form-group">
+                <input type="email" placeholder="Email Address" name="email"/>
+                <small class="form-text">This site uses Gravatar so if you want a profile image, use a Gravatar email</small >
+            </div>
+            <div class="form-group">
+                <input type="password" placeholder="Password" name="password" minLength="6"/>
+            </div>
+            <div class="form-group">
+                <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    name="password2"
+                    minLength="6"/>
+            </div>
+            <input type="submit" class="btn btn-primary" value="Register"/>
+        </form>
+        <p class="my-1">
+            Already have an account?
+            <a href="login.html">Sign In</a>
+        </p>
+
+    </Fragment>
+
+};
 
 export default Register;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,71 +1,36 @@
-import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-// import { logout } from '../../actions/auth';
+import React from "react";
 
-const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
-  const authLinks = (
-    <ul>
-      <li>
-        <Link to='/profiles'>Developers</Link>
-      </li>
-      <li>
-        <Link to='/posts'>Posts</Link>
-      </li>
-      <li>
-        <Link to='/dashboard'>
-          <i className='fas fa-user' />{' '}
-          <span className='hide-sm'>Dashboard</span>
-        </Link>
-      </li>
-      <li>
-        <a onClick={logout} href='#!'>
-          <i className='fas fa-sign-out-alt' />{' '}
-          <span className='hide-sm'>Logout</span>
-        </a>
-      </li>
-    </ul>
-  );
+const Navbar = () => {
+    return (
+        <nav className="navbar bg-dark">
+            <h1>
+                <a href="index.html">
+                    <i className="fas fa-code"></i>
+                    DevConnector</a>
+            </h1>
+            <ul>
+                <li>
+                    <Link to='/profiles'>Developers</Link>
+                </li>
+                <li>
+                    <Link to='/posts'>Posts</Link>
+                </li>
+                <li>
+                    <Link to='/dashboard'>
+                        <i className='fas fa-user'/>{' '}
+                        <span className='hide-sm'>Dashboard</span>
+                    </Link>
+                </li>
+                <li>
+                    {/* <a onClick={logout} href='#!'>
+                        <i className='fas fa-sign-out-alt'/>{' '}
+                        <span className='hide-sm'>Logout</span>
+                    </a> */}
+                </li>
+            </ul>
 
-  const guestLinks = (
-    <ul>
-      <li>
-        <Link to='/profiles'>Developers</Link>
-      </li>
-      <li>
-        <Link to='/register'>Register</Link>
-      </li>
-      <li>
-        <Link to='/login'>Login</Link>
-      </li>
-    </ul>
-  );
+        </nav>
+    )
+}
 
-  return (
-    <nav className='navbar bg-dark'>
-      <h1>
-        <Link to='/'>
-          <i className='fas fa-code' /> DevConnector
-        </Link>
-      </h1>
-      {!loading && (
-        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
-      )}
-    </nav>
-  );
-};
-
-Navbar.propTypes = {
-  logout: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
-};
-
-const mapStateToProps = state => ({
-  auth: state.auth
-});
-
-export default connect(
-  mapStateToProps,
-  { logout }
-)(Navbar);
+export default Navbar;
