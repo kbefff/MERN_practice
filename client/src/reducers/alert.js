be51d68ef18996bc9f6234f18450fadf2a0e70a2
kbefff <kbeffel10@me.com>
2019-05-24 14:44:18 -0700

    profile

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,18 +1,16 @@
 import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 
-const initialState = [
-    // {     id: 1,     msg: "Please log in",     alertType: "success" }
-];
-export default function (state = initialState, action) {
+const initialState = [];
 
-    const { type, payload } = action;
+export default function(state = initialState, action) {
+  const { type, payload } = action;
 
-    switch(type){
-        case SET_ALERT:
-            return [...state, payload];
-        case REMOVE_ALERT:
-            return state.filter(alert => alert.id !== payload);
-        default:
-            return state;
-    }
+  switch (type) {
+    case SET_ALERT:
+      return [...state, payload];
+    case REMOVE_ALERT:
+      return state.filter(alert => alert.id !== payload);
+    default:
+      return state;
+  }
 }
--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,5 @@
-import { createStore, applyMiddleware} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
@@ -8,9 +8,9 @@ const initialState = {};
 const middleware = [thunk];
 
 const store = createStore(
-    rootReducer,
-    initialState,
-    composeWithDevTools(applyMiddleware(...middleware))
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
