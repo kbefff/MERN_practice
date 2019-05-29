38211134eeef5c758ffc0ac2b3ff3248fa6ec8b2
kbefff <kbeffel10@me.com>
2019-05-29 16:11:26 -0700

    remove posts

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -10,8 +10,8 @@ import AddExperience from '../profile-forms/AddExperience';
 import AddEducation from '../profile-forms/AddEducation';
 import Profiles from '../profiles/Profiles';
 import Profile from '../profile/Profile';
-import Posts from '../posts/Posts';
-import Post from '../post/Post';
+// import Posts from '../posts/Posts';
+// import Post from '../post/Post';
 import NotFound from '../layout/NotFound';
 import PrivateRoute from '../routing/PrivateRoute';
 
@@ -29,8 +29,8 @@ const Routes = () => {
         <PrivateRoute exact path='/edit-profile' component={EditProfile} />
         <PrivateRoute exact path='/add-experience' component={AddExperience} />
         <PrivateRoute exact path='/add-education' component={AddEducation} />
-        <PrivateRoute exact path='/posts' component={Posts} />
-        <PrivateRoute exact path='/posts/:id' component={Post} />
+        {/* <PrivateRoute exact path='/posts' component={Posts} /> */}
+        {/* <PrivateRoute exact path='/posts/:id' component={Post} /> */}
         <Route component={NotFound} />
       </Switch>
     </section>
